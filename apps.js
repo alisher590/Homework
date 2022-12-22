1929dbf6071632255a77817c5ade9617161be157
алишер <aliser@MacBook-Air-aliser.local>
2022-12-22 03:42:27 -0800

    first commit

---
+++ apps.js
@@ -0,0 +1,260 @@
+let students = [
+    {
+        name: "Shakhboz",
+        age: 18,
+        gender: "m"
+    },
+    {
+        name: "Alexandr",
+        age: 32,
+        gender: "m"
+    },
+    {
+        name: "Daler",
+        age: 15,
+        gender: "m"
+    },
+    {
+        name: "Muzaffar",
+        age: 19,
+        gender: "m"
+    },
+    {
+        name: "Eldor",
+        age: 16,
+        gender: "m"
+    },
+    {
+        name: "Ibragim",
+        age: 14,
+        gender: "w"
+    },
+    {
+        name: "Ravshan",
+        age: 18,
+        gender: "m"
+    },
+    {
+        name: "Timur",
+        age: 19,
+        gender: "w"
+    },
+    {
+        name: "Aziz",
+        age: 15,
+        gender: "w"
+    },
+    {
+        name: "Darya",
+        age: 26,
+        gender: "w"
+    },
+    {
+        name: "Mukhlisa",
+        age: 16,
+        gender: "w"
+    }
+]
+
+let name = prompt ('Any name?')
+
+if(name == students[0].name){
+    console.log(
+        students[0]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[0].age.name.gender
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[0]
+            )
+        }
+}
+if(name == students[1].name){
+    console.log(
+        students[1]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[1].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[1]
+            )
+        }
+}
+if(name == students[2].name){
+    console.log(
+        students[2]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[2].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[2]
+            )
+        }
+}
+if(name == students[3].name){
+    console.log(
+        students[3]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[3].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[3]
+            )
+        }
+}
+if(name == students[4].name){
+    console.log(
+        students[4]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[4].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[4]
+            )
+        }
+}
+if(name == students[5].name){
+    console.log(
+        students[5]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[5].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[5]
+            )
+        }
+}
+if(name == students[6].name){
+    console.log(
+        students[6]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[6].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[6]
+            )
+        }
+}
+if(name == students[7].name){
+    console.log(
+        students[7]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[7].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[7]
+            )
+        }
+}
+if(name == students[8].name){
+    console.log(
+        students[8]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[8].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[8]
+            )
+        }
+}
+if(name == students[9].name){
+    console.log(
+        students[9]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[9].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[9]
+            )
+        }
+}
+if(name == students[10].name){
+    console.log(
+        students[10]
+    )
+
+        let say = confirm("Удалить этого пользователя")
+        if(say){
+            console.log('undefined')
+            delete students[10].age.name.gender
+            
+        }
+        else {
+            students.splice(0,0)
+            console.log(
+                students[10]
+            )
+        }
+}
+
+
+
